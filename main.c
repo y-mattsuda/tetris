dd73884cf7de6c8e332e210c5e3d2290bd4ac393
y-mattsuda <m.youichi1225@icloud.com>
2023-10-08 02:10:21 +0900

    refactor: 関数や構造体に分けた

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 #include <time.h>
 
 #define clearScreen() printf("\033[2J")
-#define setPosition(x, y) printf("\033[%d;%dH",(y),(x))
+// セル単位で座標をセット, (0,0)始まり
+#define setPosition(x, y) printf("\033[%d;%dH",(y)+1,(x)*2+1)
 #define setCharColor(n) printf("\033[3%dm",(n))
 #define setBackColor(n) printf("\033[4%dm",(n))
+#define setAttribute(n) printf("\033[%dm", (n))
+#define cursorOn() printf("\033[?25h")
+#define cursorOff() printf("\033[?25l")
+// Color
 #define BLACK 0
 #define RED 1
 #define GREEN 2
@@ -14,7 +19,7 @@
 #define CYAN 6
 #define WHITE 7
 #define DEFAULT 9
-#define setAttribute(n) printf("\033[%dm", (n))
+// Attribute
 #define NORMAL 0
 #define BLIGHT 1
 #define DIM 2
@@ -23,34 +28,84 @@
 #define REVERSE 7
 #define HIDE 8
 #define STRIKE 9
+// Field setting
+#define WIDTH 10
+#define HEIGHT 20
 
+typedef struct cell {
+    char c; // 表示文字
+    int charColor; // 表示色
+    int backColor; // 背景色
+    int attribute; // 文字属性
+} Cell;
+
+void initialize(void); // 画面の初期化
+void reset(void); // 画面の復元
 int wait(int msec);
+int checkRange(Cell c, int x, int y);
+int printCell(Cell c, int x, int y);
+int clearCell(Cell c, int x, int y);
 
 int main() {
     int y;
-    clearScreen();
-    for (y=1; y<23; y++) {
-        // セルを表示
-        setPosition(5, y);
-        setCharColor(WHITE);
-        setBackColor(BLACK);
-        setAttribute(REVERSE);
-        printf("  ");
-        fflush(stdout);
-        wait(500); // 500msec待つ
-        setPosition(5, y);
-        setCharColor(WHITE);
-        setBackColor(BLACK);
-        setAttribute(NORMAL);
-        printf("  ");
-        fflush(stdout);
+    Cell a = {' ', WHITE, BLACK, REVERSE};
+    initialize();
+    for (y=1; y<HEIGHT; y++) {
+        printCell(a, 5, y);
+        wait(500);
+        clearCell(a, 5, y);
     }
-    setBackColor(DEFAULT);
-    setCharColor(DEFAULT);
+    reset();
+}
+
+void initialize(void) {
+    setBackColor(BLACK);
+    setCharColor(WHITE);
+    setAttribute(NORMAL);
     clearScreen();
+    cursorOff();
+}
+
+void reset(void) {
+    setBackColor(BLACK);
+    setCharColor(WHITE);
+    setAttribute(NORMAL);
+    clearScreen();
+    cursorOn();
 }
 
 int wait(int msec) {
     struct timespec r = {0, msec * 1000L * 1000L};
     return nanosleep(&r, NULL);
 }
+
+int checkRange(Cell c, int x, int y) {
+    if (c.c=='\0' || x<0 || y<0 || x>=WIDTH || y>=HEIGHT)
+        return -1;
+    else
+        return 0;
+}
+
+int printCell(Cell c, int x, int y) {
+    if (checkRange(c, x, y) == -1)
+        return -1;
+    setPosition(x, y);
+    setAttribute(c.attribute);
+    setBackColor(c.backColor);
+    setCharColor(c.charColor);
+    printf("%c%c", c.c, c.c);
+    fflush(stdout);
+    return 0;
+};
+
+int clearCell(Cell c, int x, int y) {
+    if (checkRange(c, x, y) == -1)
+        return -1;
+    setPosition(x, y);
+    setAttribute(NORMAL);
+    setBackColor(BLACK);
+    setCharColor(BLACK);
+    printf("  ");
+    fflush(stdout);
+    return 0;
+};
