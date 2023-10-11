8c099708cb80027a0cd16bb1ec391468caec2b56
y-mattsuda <m.youichi1225@icloud.com>
2023-10-12 01:17:31 +0900

    feat: blockを表示できるように

--- main.c
+++ main.c
@@ -31,6 +31,9 @@
 // Field setting
 #define WIDTH 10
 #define HEIGHT 20
+// Block
+#define BLOCK_SIZE 4
+#define BLOCK_NUM 7
 
 typedef struct cell {
     char c; // 表示文字
@@ -39,21 +42,149 @@ typedef struct cell {
     int attribute; // 文字属性
 } Cell;
 
+// 7種類のブロックのマスタデータ
+// 1種類のブロック形状に対して4*4の16個分のCellデータが16行分、それが7種類
+Cell block_type[BLOCK_NUM][BLOCK_SIZE][BLOCK_SIZE] = {
+        '\0', RED, BLACK, NORMAL,
+        '\0', RED, BLACK, NORMAL,
+        ' ', RED, BLACK, REVERSE,
+        '\0', RED, BLACK, NORMAL,
+        '\0', RED, BLACK, NORMAL,
+        ' ', RED, BLACK, REVERSE,
+        ' ', RED, BLACK, REVERSE,
+        '\0', RED, BLACK, NORMAL,
+        '\0', RED, BLACK, NORMAL,
+        ' ', RED, BLACK, REVERSE,
+        '\0', RED, BLACK, NORMAL,
+        '\0', RED, BLACK, NORMAL,
+        '\0', RED, BLACK, NORMAL,
+        '\0', RED, BLACK, NORMAL,
+        '\0', RED, BLACK, NORMAL,
+        '\0', RED, BLACK, NORMAL,
+
+        '\0', BLUE, BLACK, NORMAL,
+        ' ', BLUE, BLACK, REVERSE,
+        ' ', BLUE, BLACK, REVERSE,
+        '\0', BLUE, BLACK, NORMAL,
+        '\0', BLUE, BLACK, NORMAL,
+        ' ', BLUE, BLACK, REVERSE,
+        '\0', BLUE, BLACK, NORMAL,
+        '\0', BLUE, BLACK, NORMAL,
+        '\0', BLUE, BLACK, NORMAL,
+        ' ', BLUE, BLACK, REVERSE,
+        '\0', BLUE, BLACK, NORMAL,
+        '\0', BLUE, BLACK, NORMAL,
+        '\0', BLUE, BLACK, NORMAL,
+        '\0', BLUE, BLACK, NORMAL,
+        '\0', BLUE, BLACK, NORMAL,
+        '\0', BLUE, BLACK, NORMAL,
+
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+        ' ', GREEN, BLACK, REVERSE,
+        ' ', GREEN, BLACK, REVERSE,
+        '\0', GREEN, BLACK, NORMAL,
+        ' ', GREEN, BLACK, REVERSE,
+        ' ', GREEN, BLACK, REVERSE,
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+        '\0', GREEN, BLACK, NORMAL,
+
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+        ' ', YELLOW, BLACK, REVERSE,
+        ' ', YELLOW, BLACK, REVERSE,
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+        ' ', YELLOW, BLACK, REVERSE,
+        ' ', YELLOW, BLACK, REVERSE,
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+        '\0', YELLOW, BLACK, NORMAL,
+
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        ' ', CYAN, BLACK, REVERSE,
+        ' ', CYAN, BLACK, REVERSE,
+        ' ', CYAN, BLACK, REVERSE,
+        ' ', CYAN, BLACK, REVERSE,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+        '\0', CYAN, BLACK, NORMAL,
+
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+        ' ', MAGENTA, BLACK, REVERSE,
+        ' ', MAGENTA, BLACK, REVERSE,
+        ' ', MAGENTA, BLACK, REVERSE,
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+        ' ', MAGENTA, BLACK, REVERSE,
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+        '\0', MAGENTA, BLACK, NORMAL,
+
+        '\0', WHITE, BLACK, NORMAL,
+        ' ', WHITE, BLACK, REVERSE,
+        '\0', WHITE, BLACK, NORMAL,
+        '\0', WHITE, BLACK, NORMAL,
+        '\0', WHITE, BLACK, NORMAL,
+        ' ', WHITE, BLACK, REVERSE,
+        '\0', WHITE, BLACK, NORMAL,
+        '\0', WHITE, BLACK, NORMAL,
+        '\0', WHITE, BLACK, NORMAL,
+        ' ', WHITE, BLACK, REVERSE,
+        ' ', WHITE, BLACK, REVERSE,
+        '\0', WHITE, BLACK, NORMAL,
+        '\0', WHITE, BLACK, NORMAL,
+        '\0', WHITE, BLACK, NORMAL,
+        '\0', WHITE, BLACK, NORMAL,
+        '\0', WHITE, BLACK, NORMAL,
+};
+
 void initialize(void); // 画面の初期化
 void reset(void); // 画面の復元
 int wait(int msec);
 int checkRange(Cell c, int x, int y);
 int printCell(Cell c, int x, int y);
 int clearCell(Cell c, int x, int y);
+void copyBlock(Cell src[BLOCK_SIZE][BLOCK_SIZE], Cell dst[BLOCK_SIZE][BLOCK_SIZE]);
+// ブロックの表示とクリア
+// 座標(x,y)は4*4エリアの左上の座標とする
+int printBlock(Cell block[BLOCK_SIZE][BLOCK_SIZE], int x, int y);
+int clearBlock(Cell block[BLOCK_SIZE][BLOCK_SIZE], int x, int y);
 
 int main() {
-    int y;
-    Cell a = {' ', WHITE, BLACK, REVERSE};
+    Cell block[BLOCK_SIZE][BLOCK_SIZE];
+    copyBlock(block_type[1], block);
     initialize();
-    for (y=1; y<HEIGHT; y++) {
-        printCell(a, 5, y);
+    for (int y=0; y<HEIGHT-BLOCK_SIZE; y++) {
+        printBlock(block, 5, y);
         wait(500);
-        clearCell(a, 5, y);
+        clearBlock(block, 5, y);
     }
     reset();
 }
@@ -107,5 +238,25 @@ int clearCell(Cell c, int x, int y) {
     setCharColor(BLACK);
     printf("  ");
     fflush(stdout);
+    return 0;
+};
+
+void copyBlock(Cell src[BLOCK_SIZE][BLOCK_SIZE], Cell dst[BLOCK_SIZE][BLOCK_SIZE]) {
+    for (int j = 0; j < BLOCK_SIZE; ++j)
+        for (int i = 0; i < BLOCK_SIZE; ++i)
+            dst[j][i] = src[j][i];
+};
+
+int printBlock(Cell block[BLOCK_SIZE][BLOCK_SIZE], int x, int y) {
+    for (int j = 0; j < BLOCK_SIZE; ++j)
+        for (int i = 0; i < BLOCK_SIZE; ++i)
+            printCell(block[j][i], i + x, j + y);
+    return 0;
+};
+
+int clearBlock(Cell block[BLOCK_SIZE][BLOCK_SIZE], int x, int y) {
+    for (int j = 0; j < BLOCK_SIZE; ++j)
+        for (int i = 0; i < BLOCK_SIZE; ++i)
+            clearCell(block[j][i], i + x, j + y);
     return 0;
 };
